2dbafd4852e2cfe3d4edb950175cb461362647eb
서민교 <67179490+hwaun123@users.noreply.github.com>
2022-09-19 16:55:15 +0900

    VIDEO

---
+++ src/Pages/Main/App.js
@@ -0,0 +1,12 @@
+const App = () => {
+  return (
+    <>
+      aa
+      <video autoPlay muted loop style={{ width: "100%" }}>
+        <source src="videos/main_video.mp4" type="video/mp4"></source>
+      </video>
+    </>
+  );
+};
+
+export default App;
