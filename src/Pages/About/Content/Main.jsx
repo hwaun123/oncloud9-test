2a521c19cdbac0c719b4cfa4e3043f5c96a13823
서민교 <67179490+hwaun123@users.noreply.github.com>
2022-09-19 17:48:45 +0900

    ABOUT

--- src/Pages/About/Content/Main.jsx
+++ src/Pages/About/Content/Main.jsx
@@ -6,12 +6,43 @@ const Main = () => {
         <div className="about-title">
           <h1 className="about-title-key">ABOUT</h1>
           <div className="about-title-text">
-            asldkjaslkdjlaskdjlkasjdlskajdlk
+            Avant Garde Studio designs and produces luxury digital fashion NFTs
+            and continuous profits to holders. Our unique technology, AG Portal
+            allows AG NFTs to be converted into wearable assets that can be worn
+            in AG compatible digital worlds including metaverses and games.
+            Additionally, the holders will receive a continuous income stream
+            from external sales of those fashion items in the physical and
+            digital worlds.
           </div>
         </div>
         <div className="about-content">
-          <h1 className="about-content-key">// ARTIST</h1>
-          <div className="about-content-text">tlqkf</div>
+          <h1 className="about-content-key">// Artistic collection</h1>
+          <div className="about-content-text">
+            Unlike physical fashion items which you can only wear or carry to
+            show your style, AG’s digital fashion items allow you to change your
+            appearance, add digital effects, wear rare wearables, and more.
+          </div>
+        </div>
+        <div className="about-content">
+          <h1 className="about-content-key">// Art concept</h1>
+          <div className="about-content-text">
+            Avant Garde Studio allows artists, fashion brands, and any designers
+            to use the AG Portal technology for them to design their unique
+            digital fashion NFTs and safely mint through our launchpad platform.
+            Through limitless technical and operation support from Avant Garde,
+            artists and designers will be able to show their creativity
+            continuously in digital worlds.
+          </div>
+        </div>
+        <div className="about-content">
+          <h1 className="about-content-key">/// holder benefit</h1>
+          <div className="about-content-text">
+            We believe one of the most fundamental values of NFT is its ability
+            to generate a profit from outside of the project’s community. Avant
+            Garde Studio will create an ecosystem in which the holders will
+            indefinitely be provided with a profit share of fashion items that
+            are sold individually in each digital world.
+          </div>
         </div>
       </div>
     </>
